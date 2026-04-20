42756604f5082cb96c5d0596986d634d310ca3ae
seth <seth@example.com>
2026-04-20 21:56:14 +0200

    illogical-impulse pas encore au point

---
+++ modules/illogical-impulse/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Illogical Impulse Hyprland Dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+
+    systems.url = "github:nix-systems/default-linux";
+
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    illogical-impulse-dotfiles = {
+      url = "github:xBLACKICEx/dots-hyprland?ref=tmp";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, illogical-impulse-dotfiles, systems, ... }@ inputs:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
+    in
+    {
+      legacyPackages = eachSystem (
+        system:
+        import ./pkgs {
+          pkgs = nixpkgs.legacyPackages.${system};
+        }
+      );
+      homeManagerModules.default = import ./modules self illogical-impulse-dotfiles inputs;
+    };
+}
